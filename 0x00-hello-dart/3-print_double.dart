3c194a5e38f34b4b95c903b6eb3e3bbf9c94762a
Raphaël <raphael.chemouni@gmail.com>
2023-11-01 13:28:52 +0100

    dart3

--- 0x00-hello-dart/3-print_double.dart
+++ 0x00-hello-dart/3-print_double.dart
@@ -1,5 +1,5 @@
 void main() {
   var n = 3.14159265359;
-  var truncatedNumber = double.parse(n.toStringAsFixed(2));
+  var truncatedNumber = n.toStringAsFixed(2);
   print('Double: $truncatedNumber\n');
 }
