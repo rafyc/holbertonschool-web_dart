bf978987cf901048dfbe58b3f15b2f510a998e3f
Raphaël <raphael.chemouni@gmail.com>
2023-11-06 14:33:33 +0100

    dart2

--- 0x01-dart-function/2-sumfunc.dart
+++ 0x01-dart-function/2-sumfunc.dart
@@ -7,7 +7,9 @@ int sub(int a, int b) {
 }
 
 String showFunc(int a, int b) {
-String messageAdd = 'Add $a + $b = $sum';
+  int sum = add(a, b);
+  int difference = sub(a, b);
+  String messageAdd = 'Add $a + $b = $sum';
   String messageSub = 'Sub $a - $b = $difference';
   return '$messageAdd\n$messageSub';
 }
