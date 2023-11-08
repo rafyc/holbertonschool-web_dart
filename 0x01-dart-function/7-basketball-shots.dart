8127a84b15a672fa804812b6f601fd3c226c28bd
Raphaël <raphael.chemouni@gmail.com>
2023-11-08 12:38:14 +0100

    dart7 refacto

--- 0x01-dart-function/7-basketball-shots.dart
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -1,36 +1,14 @@
+
+int calculatePoints(Map<String, int> team) {
+    int free = team['Free throws'] ?? 0;
+    int two = (team['2 pointers'] ?? 0) * 2;
+    int three = (team['3 pointers'] ?? 0) * 3;
+    return free + two + three;
+}
+
 int whoWins(Map<String, int> teamA, Map<String, int> teamB){
-    int freeA = 0;
-    int twoA = 0;
-    int threeA = 0;
-    int totalA = 0;
-    int totalB = 0;
-    teamA.forEach((key, value) {
-        if (key == 'Free throws') {
-             freeA = value * 1;
-        }
-        if (key == '2 pointers') {
-              twoA = value * 2;
-        }
-        if (key == '3 pointers') {
-              threeA = value * 3;
-        }
-         totalA = freeA + twoA + threeA;
-    });
- int freeB = 0;
-    int twoB = 0;
-    int threeB = 0;
-     teamB.forEach((key, value) {
-        if (key == 'Free throws') {
-             freeB = value * 1;
-        }
-        if (key == '2 pointers') {
-              twoB = value * 2;
-        }
-        if (key == '3 pointers') {
-              threeB = value * 3;
-        }
-         totalB = freeB + twoB + threeB;
-    });
+    int totalA = calculatePoints(teamA);
+    int totalB = calculatePoints(teamB);
 
     if (totalA > totalB)
         return 1;
