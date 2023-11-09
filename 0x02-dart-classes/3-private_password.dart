f673ca8bd3525e2af75c6c13d723e0d300c6c729
Raphaël <raphael.chemouni@gmail.com>
2023-11-09 16:49:12 +0100

    dart3

---
+++ 0x02-dart-classes/3-private_password.dart
@@ -0,0 +1,22 @@
+class _Password {
+    String password = '';
+
+    bool isValid() {
+        if (password.length < 8 || password.length > 16) {
+        return false;
+    }
+
+    final hasUppercase = RegExp(r'[A-Z]').hasMatch(password);
+    final hasLowercase = RegExp(r'[a-z]').hasMatch(password);
+    final hasNumber = RegExp(r'[0-9]').hasMatch(password);
+
+    return hasUppercase && hasLowercase && hasNumber;
+  }
+
+
+    String toString() {
+        return (
+            'Your Password is: ${password}'
+        );
+    }
+}
