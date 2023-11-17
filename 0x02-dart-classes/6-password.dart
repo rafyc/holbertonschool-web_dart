87a037a50124bbaf440ae43c1213282b560c7ae6
Raphaël <raphael.chemouni@gmail.com>
2023-11-17 09:54:19 +0100

    dart6

--- 0x02-dart-classes/6-password.dart
+++ 0x02-dart-classes/6-password.dart
@@ -1,29 +1,40 @@
 class Password {
-    String _password = '';
-    Password({required String password}) : _password = password;
-    bool isValid() {
-        if (_password.length < 8 || _password.length > 16) {
-        return false;
+  String _password = '';
+
+  Password({required String password}) : _password = password;
+
+  bool isValid() {
+
+    // Check password length
+    if (_password.length < 8 || _password.length > 16) return false;
+
+    // Check for uppercase and lowercase letters
+    bool hasUppercase = false;
+    bool hasLowercase = false;
+    for (int i = 0; i < _password.length; i++) {
+      if (_password[i].toUpperCase() != _password[i]) hasLowercase = true;
+      if (_password[i].toLowerCase() != _password[i]) hasUppercase = true;
     }
 
-    final hasUppercase = RegExp(r'[A-Z]').hasMatch(_password);
-    final hasLowercase = RegExp(r'[a-z]').hasMatch(_password);
-    final hasNumber = RegExp(r'[0-9]').hasMatch(_password);
+    // Check for numbers
+    bool hasNumber = false;
+    for (int i = 0; i < _password.length; i++) {
+      if (int.tryParse(_password[i]) != null) hasNumber = true;
+    }
 
     return hasUppercase && hasLowercase && hasNumber;
   }
 
-
-    String toString() {
-        return (
-            'Your Password is: ${_password}'
-        );
-    }
-
-    String get password => _password;
-
-    set password(String newPassword) {
-    _password = newPassword;
+  @override
+  String toString() {
+    return 'Your Password is: ${_password}';
   }
 
+  // Getter for the password
+  String get password => _password;
+
+  // Setter for the password
+  set password(String newPassword) {
+    _password = newPassword;
+  }
 }
