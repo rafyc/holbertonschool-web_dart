14e0cd9b772f42ea9fcd83e45923c0bcb11b542d
Raphaël <raphael.chemouni@gmail.com>
2023-11-17 09:37:02 +0100

    1

--- 0x02-dart-classes/5-json_to_user.dart
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -1,31 +1,31 @@
 class User {
-    String name = 'lolo';
-    int age = 1;
-    double height = 1.87;
-    int id = 0;
+  int id;
+  String name;
+  int age;
+  double height;
 
-  User({required this.name, required this.age, required this.height, required this.id});
+  User({required this.id, required this.name, required this.age, required this.height});
 
-    Map toJson() {
-        return {
-            'name': name,
-            'age': age,
-            'height' : height,
-            'id' : id,
-        };
-    }
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'age': age,
+      'height': height
+    };
+  }
 
-    static User fromJson(Map<dynamic, dynamic> userJson) {
-      return User{
-        name: userJson['name'],
-        age: userJson['age'],
-        height: userJson['height'],
-        id: userJson['id'],
-      };
-    }
+  static User fromJson(Map<dynamic, dynamic> userJson) {
+    return User(
+      id: userJson['id'],
+      name: userJson['name'],
+      age: userJson['age'],
+      height: userJson['height']
+    );
+  }
 
-    @override
-    String toString() {
-        return 'User(id : $id ,name: $name, age: $age, height: $height)';
+  @override
+  String toString() {
+    return 'User(id : $id ,name: $name, age: $age, height: $height)';
   }
 }
